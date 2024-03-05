24bb2baf8bcc9a37cefa560ca784aee6cd35aa79
OwenWard <owengward@gmail.com>
2024-03-05 09:14:33 -0800

    update the script

--- owen/cluster_scripts/Large_Fit.R
+++ owen/cluster_scripts/Large_Fit.R
@@ -96,11 +96,11 @@ small_data <- lichess_data %>%
   filter(Variant == "Standard") %>%
   filter(grepl("Rated Bullet game", Event))
 # 
-small_data <- lichess_data %>%
-  # filter(Event == "Rated Bullet game") %>%
-  # filter(TimeControl == "60+0") %>%
-  filter(Variant == "Standard") %>%
-  filter(grepl("Rated Blitz game", Event))
+# small_data <- lichess_data %>%
+#   # filter(Event == "Rated Bullet game") %>%
+#   # filter(TimeControl == "60+0") %>%
+#   filter(Variant == "Standard") %>%
+#   filter(grepl("Rated Blitz game", Event))
 
 users <- unique(small_data$Username)
 
@@ -166,13 +166,22 @@ fit3_ave$save_object(file = here(save_path, "all_rated_bullet_model.RDS"))
 
 ## create some summary plots of these results
 
+random_effect_post <- fit3_ave$draws() %>% as_draws_df() %>% 
+  select(starts_with("beta[")) %>% 
+  pivot_longer(cols = everything()) %>% 
+  mutate(param = stringr::str_extract(name, pattern = "\\d"),
+         id = stringr::str_extract(name, pattern = "\\d+]"),
+         id = stringr::str_replace(id, "\\]", ""),
+         player_id = paste0("beta[", id, "]"))
+
 players <- users
 names(players) <- paste0("beta[", 1:length(users), "]")
 
 player_labels <- as_labeller(players)
 
-mcmc_hist(fit3_ave$draws(c("mu_beta",  "gamma1", "gamma2", "sigma_alpha",
-                           "sigma_beta")),
+mcmc_hist(fit3_ave$draws(c("mu_beta",  "gamma1", "gamma2", 
+                           "sigma_1", "tau[1]", "tau[2]",
+                           "sigma_g1", "sigma_g2")),
           facet_args = list(scales = "free"))
 
 ggsave(filename = paste0(save_path, "/global_pars_all_rated_bullet_model.png"),
@@ -180,18 +189,38 @@ ggsave(filename = paste0(save_path, "/global_pars_all_rated_bullet_model.png"),
 # ggsave(filename = paste0(save_path, "/global_pars_all_rated_blitz_model.png"),
 #        width = 8, height = 8, units = "in")
 
-mcmc_hist(fit3_ave$draws("beta"),
-          facet_args = list(labeller = player_labels)) 
+theme_set(bayesplot_theme_get())
+
+random_effect_post %>% 
+  filter(param == 2) %>% 
+  ggplot(aes(value)) +
+  geom_histogram(fill = "#6497b1") +
+  facet_wrap(~player_id, scales = "free",
+             labeller = player_labels) +
+  labs(title = "Individual Winner Effects")
+
+## this is almost what I want now, just make it nicer
+
+# mcmc_hist(fit3_ave$draws("beta"),
+#           facet_args = list(labeller = player_labels)) 
 
 ggsave(filename = paste0(save_path, "/winner_pars_all_rated_bullet_model.png"),
        width = 8, height = 8, units = "in")
 # ggsave(filename = paste0(save_path, "/winner_pars_all_rated_blitz_model.png"),
 #        width = 8, height = 8, units = "in")
 
-names(players) <- paste0("alpha[", 1:length(users), "]")
-player_labels <- as_labeller(players)
-mcmc_hist(fit3_ave$draws("alpha"),
-          facet_args = list(labeller = player_labels))
+# names(players) <- paste0("alpha[", 1:length(users), "]")
+# player_labels <- as_labeller(players)
+# mcmc_hist(fit3_ave$draws("alpha"),
+#           facet_args = list(labeller = player_labels))
+
+random_effect_post %>% 
+  filter(param == 1) %>% 
+  ggplot(aes(value)) +
+  geom_histogram(fill = "#6497b1") +
+  facet_wrap(~player_id, scales = "free",
+             labeller = player_labels) +
+  labs(title = "Individual Player Effects")
 
 ggsave(filename = paste0(save_path, "/indiv_pars_all_rated_bullet_model.png"),
        width = 8, height = 8, units = "in")
