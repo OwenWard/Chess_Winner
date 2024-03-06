20909dffcf8ff1f2e0531b2853aaa9eb79f8c209
OwenWard <owengward@gmail.com>
2024-03-06 13:58:43 -0800

    update this to just save the user names in the right order first

--- owen/cluster_scripts/Large_Fit.R
+++ owen/cluster_scripts/Large_Fit.R
@@ -97,131 +97,133 @@ small_data <- lichess_data %>%
   filter(Variant == "Standard") %>%
   filter(grepl("Rated Bullet game", Event))
 # 
-# small_data <- lichess_data %>%
-#   # filter(Event == "Rated Bullet game") %>%
-#   # filter(TimeControl == "60+0") %>%
-#   filter(Variant == "Standard") %>%
-#   filter(grepl("Rated Blitz game", Event))
+small_data <- lichess_data %>%
+  # filter(Event == "Rated Bullet game") %>%
+  # filter(TimeControl == "60+0") %>%
+  filter(Variant == "Standard") %>%
+  filter(grepl("Rated Blitz game", Event))
 
 users <- unique(small_data$Username)
+
+
+
+## when players play less than 10 games
+## otherwise not needed
+users <- small_data %>%
+  group_by(Username) %>%
+  tally() %>%
+  filter(n >= 10) %>%
+  pull(Username)
 
 saveRDS(users, file = paste0(save_path, "users_bullet.RDS"))
 # saveRDS(users, file = paste0(save_path, "users_blitz.RDS"))
 
-## when players play less than 10 games
-## otherwise not needed
-users <- small_data %>% 
-  group_by(Username) %>% 
-  tally() %>% 
-  filter(n >= 10) %>% 
-  pull(Username)
-
 tidy_games <- map_dfr(users, get_hist, small_data, prev_n = 10) %>% 
   as_tibble()
 
-
-
-init_data <- tidy_games %>% 
-  mutate(WhiteElo = as.numeric(WhiteElo), 
-         BlackElo = as.numeric(BlackElo)) %>% 
-  mutate(focal_user = ifelse(focal_white == 1, White, Black)) %>% 
-  mutate(elo_diff = ifelse(focal_white == 1, 
-                           WhiteElo - BlackElo, BlackElo - WhiteElo)) %>% 
-  mutate(focal_id = match(focal_user, users)) %>% 
-  select(focal_user, focal_id, focal_white, 
-         focal_win_prop, elo_diff, focal_result) %>% 
-  group_by(focal_id) %>% 
-  mutate(ave_prop = lag(focal_win_prop, default = 0) - mean(focal_result)) %>% 
-  filter(focal_result != 0.5)
-
-cat("----------\n")
-print(dim(init_data))
-cat("----------\n")
-
-### then fit the models
-
-stan_data_ave <- list(N = nrow(init_data),
-                      J = length(users),
-                      y = init_data$focal_result,
-                      id = init_data$focal_id,
-                      colour = init_data$focal_white,
-                      elo = init_data$elo_diff,
-                      win_prop = init_data$ave_prop)
-
-
-stan_file <- here("owen", "cluster_scripts", "model_feb29.stan")
-
-mod <- cmdstan_model(stan_file)
-
-fit3_ave <- mod$sample(data = stan_data_ave,
-                       seed = 123,
-                       chains = 4,
-                       parallel_chains = 4,
-                       refresh = 100)
-
-
-## save the stan fit as not actually that large here, 
-## when no generated quantities
-
-fit3_ave$save_object(file = here(save_path, "all_rated_bullet_model.RDS"))
-# fit3_ave$save_object(file = here(save_path, "all_rated_blitz_model.RDS"))
-
-## create some summary plots of these results
-
-random_effect_post <- fit3_ave$draws() %>% as_draws_df() %>% 
-  select(starts_with("beta[")) %>% 
-  pivot_longer(cols = everything()) %>% 
-  mutate(param = stringr::str_extract(name, pattern = "\\d"),
-         id = stringr::str_extract(name, pattern = "\\d+]"),
-         id = stringr::str_replace(id, "\\]", ""),
-         player_id = paste0("beta[", id, "]"))
-
-players <- users
-names(players) <- paste0("beta[", 1:length(users), "]")
-
-player_labels <- as_labeller(players)
-
-mcmc_hist(fit3_ave$draws(c("mu_beta",  "gamma1", "gamma2", 
-                           "sigma_1", "tau[1]", "tau[2]",
-                           "sigma_g1", "sigma_g2")),
-          facet_args = list(scales = "free"))
-
-ggsave(filename = paste0(save_path, "/global_pars_all_rated_bullet_model.png"),
-                         width = 8, height = 8, units = "in")
-# ggsave(filename = paste0(save_path, "/global_pars_all_rated_blitz_model.png"),
+# 
+# 
+# init_data <- tidy_games %>% 
+#   mutate(WhiteElo = as.numeric(WhiteElo), 
+#          BlackElo = as.numeric(BlackElo)) %>% 
+#   mutate(focal_user = ifelse(focal_white == 1, White, Black)) %>% 
+#   mutate(elo_diff = ifelse(focal_white == 1, 
+#                            WhiteElo - BlackElo, BlackElo - WhiteElo)) %>% 
+#   mutate(focal_id = match(focal_user, users)) %>% 
+#   select(focal_user, focal_id, focal_white, 
+#          focal_win_prop, elo_diff, focal_result) %>% 
+#   group_by(focal_id) %>% 
+#   mutate(ave_prop = lag(focal_win_prop, default = 0) - mean(focal_result)) %>% 
+#   filter(focal_result != 0.5)
+# 
+# cat("----------\n")
+# print(dim(init_data))
+# cat("----------\n")
+# 
+# ### then fit the models
+# 
+# stan_data_ave <- list(N = nrow(init_data),
+#                       J = length(users),
+#                       y = init_data$focal_result,
+#                       id = init_data$focal_id,
+#                       colour = init_data$focal_white,
+#                       elo = init_data$elo_diff,
+#                       win_prop = init_data$ave_prop)
+# 
+# 
+# stan_file <- here("owen", "cluster_scripts", "model_feb29.stan")
+# 
+# mod <- cmdstan_model(stan_file)
+# 
+# fit3_ave <- mod$sample(data = stan_data_ave,
+#                        seed = 123,
+#                        chains = 4,
+#                        parallel_chains = 4,
+#                        refresh = 100)
+# 
+# 
+# ## save the stan fit as not actually that large here, 
+# ## when no generated quantities
+# 
+# fit3_ave$save_object(file = here(save_path, "all_rated_bullet_model.RDS"))
+# # fit3_ave$save_object(file = here(save_path, "all_rated_blitz_model.RDS"))
+# 
+# ## create some summary plots of these results
+# 
+# random_effect_post <- fit3_ave$draws() %>% as_draws_df() %>% 
+#   select(starts_with("beta[")) %>% 
+#   pivot_longer(cols = everything()) %>% 
+#   mutate(param = stringr::str_extract(name, pattern = "\\d"),
+#          id = stringr::str_extract(name, pattern = "\\d+]"),
+#          id = stringr::str_replace(id, "\\]", ""),
+#          player_id = paste0("beta[", id, "]"))
+# 
+# players <- users
+# names(players) <- paste0("beta[", 1:length(users), "]")
+# 
+# player_labels <- as_labeller(players)
+# 
+# mcmc_hist(fit3_ave$draws(c("mu_beta",  "gamma1", "gamma2", 
+#                            "sigma_1", "tau[1]", "tau[2]",
+#                            "sigma_g1", "sigma_g2")),
+#           facet_args = list(scales = "free"))
+# 
+# ggsave(filename = paste0(save_path, "/global_pars_all_rated_bullet_model.png"),
+#                          width = 8, height = 8, units = "in")
+# # ggsave(filename = paste0(save_path, "/global_pars_all_rated_blitz_model.png"),
+# #        width = 8, height = 8, units = "in")
+# 
+# theme_set(bayesplot_theme_get())
+# 
+# random_effect_post %>% 
+#   filter(param == 2) %>% 
+#   ggplot(aes(value)) +
+#   geom_histogram(fill = "#6497b1", colour = "black", size = 0.2) +
+#   facet_wrap(~player_id, scales = "free",
+#              labeller = player_labels) +
+#   labs(title = "Individual Winner Effects", y = "") +
+#   theme(axis.text.y = element_blank(),
+#         axis.ticks.y = element_blank())
+# 
+# 
+# # mcmc_hist(fit3_ave$draws("beta"),
+# #           facet_args = list(labeller = player_labels)) 
+# 
+# ggsave(filename = paste0(save_path, "/winner_pars_all_rated_bullet_model.png"),
 #        width = 8, height = 8, units = "in")
-
-theme_set(bayesplot_theme_get())
-
-random_effect_post %>% 
-  filter(param == 2) %>% 
-  ggplot(aes(value)) +
-  geom_histogram(fill = "#6497b1", colour = "black", size = 0.2) +
-  facet_wrap(~player_id, scales = "free",
-             labeller = player_labels) +
-  labs(title = "Individual Winner Effects", y = "") +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank())
-
-
-# mcmc_hist(fit3_ave$draws("beta"),
-#           facet_args = list(labeller = player_labels)) 
-
-ggsave(filename = paste0(save_path, "/winner_pars_all_rated_bullet_model.png"),
-       width = 8, height = 8, units = "in")
-# ggsave(filename = paste0(save_path, "/winner_pars_all_rated_blitz_model.png"),
+# # ggsave(filename = paste0(save_path, "/winner_pars_all_rated_blitz_model.png"),
+# #        width = 8, height = 8, units = "in")
+# 
+# 
+# random_effect_post %>% 
+#   filter(param == 1) %>% 
+#   ggplot(aes(value)) +
+#   geom_histogram(fill = "#6497b1", colour = "black", size = 0.2) +
+#   facet_wrap(~player_id, scales = "free",
+#              labeller = player_labels) +
+#   labs(title = "Individual Player Effects")
+# 
+# ggsave(filename = paste0(save_path, "/indiv_pars_all_rated_bullet_model.png"),
 #        width = 8, height = 8, units = "in")
-
-
-random_effect_post %>% 
-  filter(param == 1) %>% 
-  ggplot(aes(value)) +
-  geom_histogram(fill = "#6497b1", colour = "black", size = 0.2) +
-  facet_wrap(~player_id, scales = "free",
-             labeller = player_labels) +
-  labs(title = "Individual Player Effects")
-
-ggsave(filename = paste0(save_path, "/indiv_pars_all_rated_bullet_model.png"),
-       width = 8, height = 8, units = "in")
-# ggsave(filename = paste0(save_path, "/indiv_pars_all_rated_blitz_model.png"),
-#        width = 8, height = 8, units = "in")
+# # ggsave(filename = paste0(save_path, "/indiv_pars_all_rated_blitz_model.png"),
+# #        width = 8, height = 8, units = "in")
