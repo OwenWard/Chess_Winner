3f0de4a7ad8fcb53de44187ed3736b47c32f5895
OwenWard <owengward@gmail.com>
2024-03-26 11:19:17 -0700

    save in a new folder to ensure they were rerun

--- owen/cluster_scripts/Large_Fit.R
+++ owen/cluster_scripts/Large_Fit.R
@@ -29,13 +29,13 @@ all_save_path <- rep(NA, 3)
 all_data_path[1] <- here("box_data/lichess1700-1900/")
 all_data_path[2] <- here("box_data/lichess2000-2200/")
 all_data_path[3] <- here("box_data/lichess2300-2500/")
-all_save_path[1] <- here("results/lichess1700-1900_feb/")
+all_save_path[1] <- here("results/lichess1700-1900_mar/")
 
 # all_save_path[1] <- here("results/Full_Fits/lichess1700-1900/")
 # ## if need to run it locally
 
-all_save_path[2] <- here("results/lichess2000-2200_feb/")
-all_save_path[3] <- here("results/lichess2300-2500_feb/")
+all_save_path[2] <- here("results/lichess2000-2200_mar/")
+all_save_path[3] <- here("results/lichess2300-2500_mar/")
 
 
 data_path <- all_data_path[path_id]
