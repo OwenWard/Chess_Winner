5bcdd498199aea3fc1dc2917bf226c0bfb7e1ce9
OwenWard <owengward@gmail.com>
2024-06-24 10:04:16 -0700

    update these for blitz for the gm data

--- owen/cluster_scripts/Large_Single_Prev.R
+++ owen/cluster_scripts/Large_Single_Prev.R
@@ -58,21 +58,19 @@ lichess_data <- files %>%
 ## restrict to rated rapid and shorter here
 ## this also removes the NAs, which makes sense
 
-patterns <- c("Rated Bullet game", "Rated bullet game")
 
-small_data <- lichess_data %>%
-  # filter(Event == "Rated Bullet game") %>%
-  mutate(Event = tolower(Event)) |> 
-  filter(TimeControl == "60+0") %>%
-  filter(Variant == "Standard") %>%
-  filter(grepl("rated bullet game", Event))
- 
 # small_data <- lichess_data %>%
-#   mutate(Event = tolower(Event)) |> 
 #   # filter(Event == "Rated Bullet game") %>%
-#   filter(TimeControl == "180+0") %>%
+#   mutate(Event = tolower(Event)) |> 
+#   filter(TimeControl == "60+0") %>%
 #   filter(Variant == "Standard") %>%
-#   filter(grepl("rated blitz game", Event))
+#   filter(grepl("rated bullet game", Event))
+ 
+small_data <- lichess_data %>%
+  mutate(Event = tolower(Event)) |>
+  filter(TimeControl == "180+0") %>%
+  filter(Variant == "Standard") %>%
+  filter(grepl("rated blitz game", Event))
 
 users <- unique(small_data$Username)
 
@@ -84,8 +82,8 @@ users <- small_data %>%
   filter(n >= 10) %>% 
   pull(Username)
 
-saveRDS(users, file = paste0(save_path, "users_bullet.RDS"))
-# saveRDS(users, file = paste0(save_path, "users_blitz.RDS"))
+# saveRDS(users, file = paste0(save_path, "users_bullet.RDS"))
+saveRDS(users, file = paste0(save_path, "users_blitz.RDS"))
 
 tidy_games <- map_dfr(users, get_hist, small_data, prev_n = 10) %>%  
   as_tibble()
@@ -143,8 +141,8 @@ fit3_ave <- mod$sample(data = stan_data_ave,
 
 ## save the stan fit as not actually that large here
 
-fit3_ave$save_object(file = here(save_path, "all_rated_bullet_model_prev.RDS"))
-# fit3_ave$save_object(file = here(save_path, "all_rated_blitz_model_prev.RDS"))
+# fit3_ave$save_object(file = here(save_path, "all_rated_bullet_model_prev.RDS"))
+fit3_ave$save_object(file = here(save_path, "all_rated_blitz_model_prev.RDS"))
 
 ## create some summary plots of these results
 
@@ -166,12 +164,12 @@ mcmc_hist(fit3_ave$draws(c("mu_beta",  "gamma1", "gamma2",
                            "sigma_g1", "sigma_g2")),
           facet_args = list(scales = "free"))
 
-ggsave(filename = paste0(save_path,
-                         "/global_pars_all_rated_bullet_model_prev.png"),
-                         width = 8, height = 8, units = "in")
 # ggsave(filename = paste0(save_path,
-#                          "/global_pars_all_rated_blitz_model_prev.png"),
-#        width = 8, height = 8, units = "in")
+#                          "/global_pars_all_rated_bullet_model_prev.png"),
+#                          width = 8, height = 8, units = "in")
+ggsave(filename = paste0(save_path,
+                         "/global_pars_all_rated_blitz_model_prev.png"),
+       width = 8, height = 8, units = "in")
 
 theme_set(bayesplot_theme_get())
 
@@ -185,12 +183,12 @@ random_effect_post %>%
   theme(axis.text.y = element_blank(),
         axis.ticks.y = element_blank())
 
-ggsave(filename = paste0(save_path,
-                         "/winner_pars_all_rated_bullet_model_prev.png"),
-       width = 8, height = 8, units = "in")
 # ggsave(filename = paste0(save_path,
-#                          "/winner_pars_all_rated_blitz_model_prev.png"),
+#                          "/winner_pars_all_rated_bullet_model_prev.png"),
 #        width = 8, height = 8, units = "in")
+ggsave(filename = paste0(save_path,
+                         "/winner_pars_all_rated_blitz_model_prev.png"),
+       width = 8, height = 8, units = "in")
 
 random_effect_post %>% 
   filter(param == 1) %>% 
@@ -201,9 +199,9 @@ random_effect_post %>%
   labs(title = "Individual Player Effects")
 
 
-ggsave(filename = paste0(save_path,
-                         "/indiv_pars_all_rated_bullet_model_prev.png"),
-       width = 8, height = 8, units = "in")
 # ggsave(filename = paste0(save_path,
-#                          "/indiv_pars_all_rated_blitz_model_prev.png"),
+#                          "/indiv_pars_all_rated_bullet_model_prev.png"),
 #        width = 8, height = 8, units = "in")
+ggsave(filename = paste0(save_path,
+                         "/indiv_pars_all_rated_blitz_model_prev.png"),
+       width = 8, height = 8, units = "in")
