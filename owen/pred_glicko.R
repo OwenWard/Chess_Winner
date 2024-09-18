e663fa55dee34e3919ce596fb0230c466f83f844
OwenWard <owengward@gmail.com>
2024-09-18 14:28:49 -0700

    tidy this up quite a bit

--- owen/pred_glicko.R
+++ owen/pred_glicko.R
@@ -1,5 +1,7 @@
-##### trying to do the ppc for the lichess rating
-
+#### Sept 18th 2024
+#### Construc a PPC of the evolution of the Lichess rating over time
+#### Using complete simulated trajectories of their final 1000 games,
+#### using parameter estimats from model fit to first 1000 games
 
 library(PlayerRatings)
 library(tidyverse)
@@ -42,111 +44,87 @@ res_data_path[2] <- here("results/lichess2000-2200/")
 res_data_path[3] <- here("results/lichess2300-2500/")
 res_data_path[4] <- here("results/lichessGrandmasters/")
 
-# dir.create(save_path, showWarnings = FALSE)
 
 files <- list.files(data_path)
 
-lichess_data <- files %>% 
+lichess_data <- files |> 
   map_dfr(~read_player(data_path, .x))
 
 
-small_data <- lichess_data %>%
+small_data <- lichess_data |>
   mutate(Event = tolower(Event)) |> 
-  filter(TimeControl == "60+0") %>%
-  filter(Variant == "Standard") %>%
+  filter(TimeControl == "60+0") |>
+  filter(Variant == "Standard") |>
   filter(grepl("rated bullet game", Event))
 
 rm(lichess_data)
 
-
-
-select_users <- small_data %>% 
-  group_by(Username) %>% 
-  tally() %>% 
-  arrange(-n) %>% 
-  slice_max(order = n, n = 10) %>% 
+select_users <- small_data |> 
+  group_by(Username) |> 
+  tally() |> 
+  arrange(-n) |> 
+  slice_max(order = n, n = 10) |> 
   pull(Username)
 
 
-## just use the first for now
 users <- select_users
 
 
-last_games <- small_data %>% 
-  filter(Username %in% users) %>% 
-  group_by(Username) %>% 
-  arrange(UTCDate, UTCTime, .by_group = TRUE) %>% 
-  slice_tail(n = 1000) %>% 
-  ungroup() %>% 
-  group_by(Username) %>% 
-  mutate(Result = sample(Result)) %>% 
+## will use last 1000 games each user plays
+
+last_games <- small_data |> 
+  filter(Username %in% users) |> 
+  group_by(Username) |> 
+  arrange(UTCDate, UTCTime, .by_group = TRUE) |> 
+  slice_tail(n = 1000) |> 
+  ungroup() |> 
+  group_by(Username) |> 
+  mutate(Result = sample(Result)) |> 
   ungroup()
 
 
-tidy_games <- map_dfr(users, get_hist, last_games, prev_n = 1) %>% 
+tidy_games <- map_dfr(users, get_hist, last_games, prev_n = 1) |> 
   as_tibble()
 
 
-init_data <- tidy_games %>% 
+init_data <- tidy_games |> 
   mutate(WhiteElo = as.numeric(WhiteElo), 
-         BlackElo = as.numeric(BlackElo)) %>% 
-  mutate(focal_user = ifelse(focal_white == 1, White, Black)) %>% 
+         BlackElo = as.numeric(BlackElo)) |> 
+  mutate(focal_user = ifelse(focal_white == 1, White, Black)) |> 
   mutate(elo_diff = ifelse(focal_white == 1, 
-                           WhiteElo - BlackElo, BlackElo - WhiteElo)) %>% 
-  mutate(focal_id = match(focal_user, users)) %>% 
-  # select(focal_user, focal_id, focal_white, 
-  #        focal_win_prop, elo_diff, focal_result) %>% 
-  group_by(focal_id) %>%
-  mutate(ave_prop = lag(focal_win_prop, default = 0) - mean(focal_win_prop)) %>%
+                           WhiteElo - BlackElo, BlackElo - WhiteElo)) |> 
+  mutate(focal_id = match(focal_user, users)) |> 
+  group_by(focal_id)  |> 
+  mutate(ave_prop = lag(focal_win_prop, default = 0) - mean(focal_win_prop)) |>
   filter(focal_result != 0.5)
 
 
 
-## need a function which will take in a set of games and results
-## and return the corresponding glicko2 scores for the focal players
 
-## first do it for a single player, might be easier
-## don't know the deviation, volatility used by lichess so just need
-## to guess these
-## can take the true elo for opponents throughout, to make it closer?
+## get their starting scores for each player
 
-## get starting rating of focal player also
-## starting in the wrong place here
-focal_init <- tidy_games |> 
-  slice_head(n = 1) |>
-  mutate(Player = ifelse(White %in% users, White, Black),
-         Rating = ifelse(White %in% users, WhiteElo, BlackElo)) |> 
-  select(Player, Rating)
-
-init_data |> group_by(focal_user) |> 
-  slice_head(n = 1) |> 
-  arrange(focal_id)
 
 
 ## then just pick the player from this data
-### need to know the focal id
+## need to choose the focal id and format for the glicko2 function
 
 focal_player_used <- 1
 
-focal_init <- tibble(Player = "d8336",
-                     Rating = 1881)
-
-# init_data |> 
-#   slice_head(n = 1) |> 
-  ## then get the id of that user and use it below
-  ## along with their starting elo
-  ### get just the games from the last user,
-  ## then their last 1001 games,
-  ## then take the elo from the FIRST game from those
+focal_init <- init_data |> group_by(focal_user) |> 
+  slice_head(n = 1) |> 
+  arrange(focal_id) |> 
+  filter(focal_id == focal_player_used) |> 
+  mutate(focal_elo = ifelse(focal_user == White, WhiteElo, BlackElo)) |> 
+  select(focal_user, focal_elo) |> 
+  rename(Player = focal_user, Rating = focal_elo)
 
 
-focal_user <- focal_init$Player
 
 glick_data <- init_data |>
   filter(focal_user == focal_init$Player)
-  # mutate(Username = focal_user) |> 
-# |> 
-#   filter(Username == focal_init$Player)
+
+
+# First compare computed Glicko with Lichess ------------------------------
 
 
 ## first argument to glicko2
@@ -170,11 +148,10 @@ glick_part2 <- glick_data |>
   select(opp, opp_elo) |> 
   rename(Player = opp, Rating = opp_elo) 
 
+## need to select the Deviation (to start with) and Volatility 
 
 glick_part2 <- bind_rows(focal_init, glick_part2) |> 
   mutate(Deviation = 50, Volatility = 0.05)
-
-
 
 initstate <- glick_part2
 
@@ -192,10 +169,7 @@ glick_scores <- as.numeric(focal_glick[, "Rating"])
 
 plot(glick_scores, type = "l")
 
-
-
-
-# first try match these to the lichess scores a little -------------------------
+## plot against lichess estimates
 
 true_elo <- last_games |> 
   filter(Username == focal_init$Player) |>
@@ -215,129 +189,37 @@ true_elo |>
   geom_line()
 
 
-## not super close but will have to do for now
-## could be the missing draws which are messing it up here also
 
-# then repeat using the simulations ---------------------------------------
-
-
-
-## take in the simulation results and compare them here
-
+# Setup to Simulate Complete Trajectory of Games and Scores ---------------------------------------
 
 data_path <- res_data_path[path_id]
 
+
+## get the draws from the first
 fit_17_19_first <- readRDS(file =  here(data_path, "time",
                                        "select_users_first_bullet.RDS"))
 
 fit_17_19_last <- readRDS(file =  here(data_path, "time",
                                        "select_users_last_bullet.RDS"))
 
-fit_samples <- as_draws_df(fit_17_19_last$draws())
-
-fit_samp <- fit_samples %>% 
-  select(!starts_with(c("log_lik", "yrep")))
-
-y_rep <- fit_samples %>% select(starts_with("y_rep"))
-
-rm(fit_samples)
-rm(fit_samp)
-
-stan_data_ave_last <- readRDS(file = here(data_path, "time",
-                                           "stan_data_last.RDS"))
-
-y_rep_mod <- y_rep %>% 
-  rownames_to_column() %>%  
-  pivot_longer(-rowname) %>% 
-  pivot_wider(names_from=rowname, values_from=value) %>% 
-  ## remove the draws and chains here
-  mutate(focal_id = stan_data_ave_last$id)
 
 
-## single player
+## extract draws from model fit to first
 
-#### modify this if picking a different player!! #####
-### check the corresponding id in select_users
-### there are several places below which filter on the id == 1
-### if using other id be careful
-
-player_sims <- y_rep_mod %>% 
-  pivot_longer(cols = `1`:`4000`, names_to = "draw", values_to = "y") |> 
-  group_by(focal_id, draw) |> 
-  mutate(game_id = row_number()) |> 
-  filter(focal_id == focal_player_used)
-
-
-player_sims |> 
-  group_by(draw) |> 
-  tally()
-
-
-## need to make sure this is matching correctly to the right focal player
-
-glicko_sims <- tibble()
-
-for(i in 1:500){
-  curr_result <- player_sims |> 
-    filter(draw == i) |> 
-    pull(y)
-  
-  ## then compute the glicko scores here 
-  games <- data.frame(Week = glick_part1$index,
-                      Player1 = focal_init$Player,
-                      Player2 = glick_part1$opp,
-                      Score = curr_result)
-  
-  sim_glick <- glicko2(games, status = initstate, history = TRUE, 
-               gamma = glick_part1$focal_white)
-  focal_glick <- sim_glick$history[focal_init$Player, ,]
-  
-  glick_scores <- as.numeric(focal_glick[, "Rating"])
-  
-  glick_tibble <- tibble(glicko = glick_scores, draw = i) |> 
-    mutate(game = row_number())
-  
-  glicko_sims <- glicko_sims |> 
-    bind_rows(glick_tibble)
-}
-
-
-## then overlay the truth
-tru_glicko <- true_elo |> 
-  rename(game = index, glicko = focal_elo)
-
-
-glicko_sims |> 
-  ggplot(aes(game, glicko)) +
-  geom_line(alpha = 0.01, mapping = aes(group = draw)) +
-  # geom_smooth() +
-  geom_line(data = tru_glicko,
-            col = "red") +
-  NULL
-
-
-## this is ppc with the model fit to the same data, rather than extended out
-## into the future. still useful but just not as good as the one we really want
-
-
-## looking at that here below
-
-## this takes the true elo diff, which explains the good performance
-
-
-draws <- fit_17_19_first$draws() %>% as_draws_df() %>%
-  select(starts_with(c("beta[", "gamma"))) %>%
-  mutate(draw = row_number()) %>% 
-  pivot_longer(cols = "beta[1,1]":"beta[2,10]") %>% 
+draws <- fit_17_19_first$draws() |> as_draws_df() |>
+  select(starts_with(c("beta[", "gamma"))) |>
+  mutate(draw = row_number()) |> 
+  pivot_longer(cols = "beta[1,1]":"beta[2,10]") |> 
   mutate(param = stringr::str_extract(name, pattern = "\\d"),
          id = stringr::str_extract(name, pattern = "\\d+]"),
          id = stringr::str_replace(id, "\\]", ""),
-         player_id = paste0("beta[", id, "]")) %>% 
-  select(-name) %>% 
-  pivot_wider(names_from = param, values_from = value) %>% 
+         player_id = paste0("beta[", id, "]")) |> 
+  select(-name) |> 
+  pivot_wider(names_from = param, values_from = value) |> 
   rename(beta1 = `1`, beta2 = `2`)
 
 
+## the covariates for the future games
 
 future_games <- tibble(focal = as.character(init_data$focal_id),
                        color = init_data$focal_white,
@@ -345,131 +227,16 @@ future_games <- tibble(focal = as.character(init_data$focal_id),
                        hist = init_data$ave_prop)
 
 
-num_draws <- 500
-
-mix <- left_join(draws |>  filter(draw <= num_draws),
-                 future_games, by = join_by("id" == "focal"),
-                 relationship = "many-to-many") %>% 
-  rowwise() %>% 
-  mutate(inv_logit = beta1 + beta2 * hist + gamma1 * color + gamma2 * elo_diff,
-         prob = exp(inv_logit)/(1 + exp(inv_logit)))
-
-
-small_sim <- mix %>% 
-  filter(id == 1) |> 
-  mutate(sim_result = sample(c(1, 0), size = 1, prob = c(prob, 1 - prob))) %>% 
-  select(id, draw, sim_result) %>% 
-  group_by(id, draw) |> 
-  mutate(game = row_number()) |> 
-  # filter(id == 1) |> 
-  rename(y = sim_result)
-
-
-
-
-## then use this to compute the glicko scores again
-
-glicko_sims_forward <- tibble()
-
-for(i in 1:500){
-  curr_result <- small_sim |> 
-    filter(draw == i) |> 
-    pull(y)
-  
-  ## then compute the glicko scores here 
-  games <- data.frame(Week = glick_part1$index,
-                      Player1 = focal_init$Player,
-                      Player2 = glick_part1$opp,
-                      Score = curr_result)
-  
-  sim_glick <- glicko2(games, status = initstate, history = TRUE, 
-                       gamma = glick_part1$focal_white)
-  focal_glick <- sim_glick$history[focal_init$Player, ,]
-  
-  glick_scores <- as.numeric(focal_glick[, "Rating"])
-  
-  glick_tibble <- tibble(glicko = glick_scores, draw = i) |> 
-    mutate(game = row_number())
-  
-  glicko_sims_forward <- glicko_sims_forward |> 
-    bind_rows(glick_tibble)
-}
-
-
-
-## then plot this version also
-
-glicko_sims_forward |> 
-  ggplot(aes(game, glicko)) +
-  geom_line(alpha = 0.01, mapping = aes(group = draw)) +
-  # geom_smooth() + 
-  geom_line(data = tru_glicko,
-            col = "red") +
-  NULL
-
-
-
-## save the glicko outputs (so could recreate the plots, modify as needed)
-## set it up so you can specify the users you want to plot 
-## and only run it for them, can do that on the cluster
-
-## add in all the history games in advance of this also, to see the comparison 
-## more broadly
-
-
-rest_games <- small_data |> 
-  filter(Username == focal_init$Player) |> 
-  arrange(UTCDate, UTCTime) |> 
-  slice(1:(n() - 1000)) |> 
-  mutate(game = row_number())
-
-
-num_prev <- nrow(rest_games)
-
-
-sim_plot_data <- glicko_sims_forward |> 
-  mutate(game = game + num_prev)
-
-plot_true <- tru_glicko |> 
-  mutate(game = game + num_prev)
-
-
-rest_games |> 
-  mutate(glicko = ifelse(Username == White, WhiteElo, BlackElo),
-         game = row_number()) |>
-  select(game, glicko) |> 
-  ggplot(aes(game, glicko, UTCDate, UTCTime)) +
-  geom_line(linewidth = 0.5, col = "darkred") + 
-  geom_line(data = sim_plot_data, mapping = aes(group = draw), alpha = 0.005) +
-  geom_line(data = plot_true,
-            col = "red", linewidth = 0.5) +
-  xlim(c(28000, num_prev + 1000)) +
-  # xlim(c(85000, num_prev + 1000)) +
-  # xlim(c(125000, num_prev + 1000)) +
-  labs(x = "Games Played", y = "Glicko Score")
-
-
-
-
-
-
-
-# Modify this to include the simulated Glicko scores ----------------------
-
-## need to do this one game at a time, simulating the results as we go
-
-
-
-
+## process for below
 
 focal_games <- future_games |> 
-  filter(focal == 1)
+  filter(focal == focal_player_used)
 
 
 full_game_info <- init_data |> 
-  filter(focal_id == 1)
+  filter(focal_id == focal_player_used)
 
-
+# slow for even 100 sims at the moment, due to the for loop
 num_sims <- 100
 
 num_games <- nrow(full_game_info)
@@ -483,30 +250,29 @@ game_sim <- data.frame(NA, nrow = num_games, ncol = num_sims)
 ## focal_win_prop = focal_result when just using previous game
 true_avg <- mean(full_game_info$focal_result)
 
+focal_draws <- draws |> 
+  filter(id == focal_player_used)
+
 for(i in 1:num_games){
   
   ## get the game information for the ith game
   game_info <- focal_games[i, ]
-  ## need to get the opponent elo from original data
-  ## and the hist needs to be correct each time
+  # gamma the colour effect for glicko calc
   gamma <- ifelse(game_info$color == 1, 1, -1)
+  
+  # get opponent name and ability
   opp_elo <- full_game_info[i,] |> 
     mutate(opp = ifelse(focal_white == 1, BlackElo, WhiteElo)) |> 
     pull(opp)
   opp <- full_game_info[i,] |> 
     mutate(opp_name = ifelse(focal_white == 1, Black, White)) |> 
     pull(opp_name)
+  
+  curr_game <- draws[]
     
   for(j in 1:num_sims){
     
-    ## get the jth sim params for that 
-    ## simulate the ith game and the corresponding glicko score
-    ## store this in glicko_sim[i, j]
-    ## need the result of the previous sim game also,
-    ## to get the hist term
-    ## can assume their average is the same here for this
-    curr_draw <- draws |> 
-      filter(draw == j & id == 1)
+    curr_draw <- focal_draws[focal_draws$draw == j, ]
     
     ## if i = 1 use the initial glicko, otherwise use the previous
     if(i == 1){
@@ -545,19 +311,83 @@ for(i in 1:num_games){
 }
 
 
-## then check it out
+## quick plot to confirm
 
-quick_sim <- glicko_sim[1:928, 1:100]  
+quick_sim <- glicko_sim[, 1:100]  
 
 colnames(quick_sim) <- paste0("Sim_", 1:100)
 
-as_tibble(quick_sim) |> 
+
+quick_sim <- as_tibble(quick_sim) |> 
   mutate(game = row_number()) |> 
   pivot_longer(cols = Sim_1:Sim_100, names_to = "Sim",
-               values_to = "glicko") |> 
+               values_to = "glicko") 
+
+quick_sim |> 
   ggplot(aes(game, glicko)) +
   geom_line(alpha = 0.05, aes(group = Sim)) +
   geom_line(data = tru_glicko,
             col = "red")
 
 
+
+# More complete plot for poster -------------------------------------------
+
+
+rest_games <- small_data |> 
+  filter(Username == focal_init$Player) |> 
+  arrange(UTCDate, UTCTime) |> 
+  slice(1:(n() - 1000)) |> 
+  mutate(game = row_number())
+
+
+num_prev <- nrow(rest_games)
+
+
+sim_plot_data <- quick_sim |> 
+  mutate(game = game + num_prev)
+
+plot_true <- tru_glicko |> 
+  mutate(game = game + num_prev)
+
+
+rest_games |> 
+  mutate(glicko = ifelse(Username == White, WhiteElo, BlackElo),
+         game = row_number()) |>
+  select(game, glicko) |> 
+  ggplot(aes(game, glicko, UTCDate, UTCTime)) +
+  geom_line(linewidth = 0.5, col = "darkred") + 
+  geom_line(data = sim_plot_data, mapping = aes(group = Sim), alpha = 0.05) +
+  geom_line(data = plot_true,
+            col = "red", linewidth = 0.5) +
+  # xlim(c(28000, num_prev + 1000)) +
+  # xlim(c(85000, num_prev + 1000)) +
+  xlim(c(125000, num_prev + 1000)) +
+  labs(x = "Games Played", y = "Glicko Score")
+
+
+
+
+# Can also plot the number of Games Won -----------------------------------
+
+## compare this to the truth for the single player
+
+
+quick_sim_games <- game_sim[, 1:100]  
+
+colnames(quick_sim_games) <- paste0("Sim_", 1:100)
+
+
+quick_sim_games <- as_tibble(quick_sim_games) |> 
+  mutate(game = row_number()) |> 
+  pivot_longer(cols = Sim_1:Sim_100, names_to = "Sim",
+               values_to = "Result") 
+
+true_games_won <- sum(full_game_info$focal_result)
+
+quick_sim_games |> 
+  group_by(Sim) |> 
+  summarise(games_won = sum(Result)) |> 
+  ggplot(aes(games_won)) +
+  geom_histogram() +
+  geom_vline(aes(xintercept = true_games_won), col = "red")
