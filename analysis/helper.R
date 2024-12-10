f9870d4f2538f951d0051aaf6d1eb6f7c4ff557d
OwenWard <owengward@gmail.com>
2024-12-10 13:09:20 -0800

    update the pipe but more importantly update the filter in the historic games, to only include games where the focal player was actually focal

--- analysis/helper.R
+++ analysis/helper.R
@@ -10,9 +10,9 @@ read_player <- function(path, file){
                                    BlackTitle = col_character(),
                                    WhiteElo = col_character(),
                                    BlackElo = col_character(),
-                                   FEN = col_character())) %>% 
+                                   FEN = col_character())) |> 
     select(Username, Event, White, Black, Result, UTCDate, UTCTime, 
-           WhiteElo, BlackElo, Variant, TimeControl, Termination) %>% 
+           WhiteElo, BlackElo, Variant, TimeControl, Termination) |> 
     mutate(WhiteElo = parse_number(if_else(WhiteElo == "?", NA, WhiteElo)),
            BlackElo = parse_number(if_else(BlackElo == "?", NA, BlackElo)))
   dat
@@ -20,32 +20,34 @@ read_player <- function(path, file){
 
 get_hist <- function(user, games, prev_n) {
   if(prev_n == 1){
-    hist_games <- games %>% 
-      filter(White == user | Black == user) %>% 
-      arrange(UTCDate, UTCTime) %>% 
-      mutate(focal_white = ifelse(Username == White, 1, 0)) %>% 
-      select(White:BlackElo, focal_white) %>% 
+    hist_games <- games |> 
+      # filter(White == user | Black == user) |>
+      filter(Username == user) |> 
+      arrange(UTCDate, UTCTime) |> 
+      mutate(focal_white = ifelse(Username == White, 1, 0)) |> 
+      select(White:BlackElo, focal_white) |> 
       mutate(focal_result = case_when(
         (focal_white == 1 & Result == "1-0") ~ 1,
         (focal_white == 0 & Result == "0-1") ~ 1,
         (Result == "1/2-1/2") ~ 0.5,
         .default = 0
-      )) %>% 
+      )) |> 
       mutate(focal_win_prop = focal_result)
     
   }
   else{
-    hist_games <- games %>% 
-      filter(White == user | Black == user) %>% 
-      arrange(UTCDate, UTCTime) %>% 
-      mutate(focal_white = ifelse(Username == White, 1, 0)) %>% 
-      select(White:BlackElo, focal_white) %>% 
+    hist_games <- games |> 
+      # filter(White == user | Black == user) |> 
+      filter(Username == user) |> 
+      arrange(UTCDate, UTCTime) |> 
+      mutate(focal_white = ifelse(Username == White, 1, 0)) |> 
+      select(White:BlackElo, focal_white) |> 
       mutate(focal_result = case_when(
         (focal_white == 1 & Result == "1-0") ~ 1,
         (focal_white == 0 & Result == "0-1") ~ 1,
         (Result == "1/2-1/2") ~ 0.5,
         .default = 0
-      )) %>% 
+      )) |> 
       mutate(focal_win_prop = c(cumsum(focal_result[1:(prev_n - 1)])/(1:(prev_n -1)), 
                                 roll_mean(focal_result, n = prev_n)))
   }
