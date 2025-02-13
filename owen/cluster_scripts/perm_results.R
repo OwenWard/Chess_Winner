fa9c5b742520fe1961c54cc740dd25a4d816227b
Owen <owengward@gmail.com>
2025-02-13 09:47:44 -0800

    update save path for this

--- owen/cluster_scripts/perm_results.R
+++ owen/cluster_scripts/perm_results.R
@@ -31,16 +31,16 @@ all_data_path[1] <- here("box_data/lichess1700-1900/")
 all_data_path[2] <- here("box_data/lichess2000-2200/")
 all_data_path[3] <- here("box_data/lichess2300-2500/")
 all_data_path[4] <- here("box_data/lichessGrandmasters/")
-all_save_path[1] <- here("results/lichess1700-1900_test/")
+all_save_path[1] <- here("results/lichess1700-1900_2025/")
 
 # all_save_path[1] <- here("results/Full_Fits/lichess1700-1900/")
 # ## if need to run it locally
 
-all_save_path[2] <- here("results/lichess2000-2200_test/")
-all_save_path[3] <- here("results/lichess2300-2500_test/")
-all_save_path[4] <- here("results/lichessGrandmasters_test/")
+all_save_path[2] <- here("results/lichess2000-2200_2025/")
+all_save_path[3] <- here("results/lichess2300-2500_2025/")
+all_save_path[4] <- here("results/lichessGrandmasters_2025/")
 
-path_id <- 1 ## setting it for now
+path_id <- 4 ## setting it for now
 data_path <- all_data_path[path_id]
 save_path <- paste0(all_save_path[path_id], "perm/")
 
