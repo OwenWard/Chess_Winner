ea161036e74dd42bba4607d049c2d7cfccb0552d
AdamGee99 <arg15@sfu.ca>
2025-04-16 14:37:28 -0700

    removing plots after we save the fit, don't use it anymore

--- analysis_scripts/01_Large_Fit.R
+++ analysis_scripts/01_Large_Fit.R
@@ -12,6 +12,7 @@
 #' Adam revision 
 #' removing the standardization of x_ij term
 #' also assuming games played in different sessions have no relation, so make their history NULL (ie same as draw)
+#' removing all the plots and stuff after we save the fit, we don't use that anymore
 
 
 
@@ -31,7 +32,7 @@ source(here("utils/helper.R"))
 
 path_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 
-n <- 10 ## number of games to use for history
+n <- 1 ## number of games to use for history
 
 ### load in the data to use
 
@@ -115,9 +116,6 @@ init_data <- tidy_games |>
                            0.5)) |>
   filter(focal_result != 0.5)
 
-cat("----------\n")
-print(dim(init_data))
-cat("----------\n")
 
 ### then fit the models
 
@@ -134,77 +132,13 @@ stan_file <- here("analysis_scripts", "final_model_scale_priors.stan")
 
 mod <- cmdstan_model(stan_file)
 
-fit3_ave <- mod$sample(data = stan_data_ave,
-                       seed = 123,
-                       chains = 4,
-                       parallel_chains = 4,
-                       refresh = 100)
+fit <- mod$sample(data = stan_data_ave,
+                  seed = 123,
+                  chains = 4,
+                  parallel_chains = 4,
+                  refresh = 100)
 
-
-## save the stan fit as not actually that large here,
-## when no generated quantities
-
-fit3_ave$save_object(file = here(save_path,
+#save fit
+fit$save_object(file = here(save_path,
                                  paste0("all_rated_bullet_model_n", n, ".RDS")))
-# fit3_ave$save_object(file = here(save_path,
-#                                  paste0("all_rated_blitz_model_n", n, ".RDS")))
 
-## create some summary plots of these results
-
-random_effect_post <- fit3_ave$draws() |> 
-  as_draws_df() |>
-  select(starts_with("beta[")) |>
-  pivot_longer(cols = everything()) |>
-  mutate(param = stringr::str_extract(name, pattern = "\\d"),
-         id = stringr::str_extract(name, pattern = "\\d+]"),
-         id = stringr::str_replace(id, "\\]", ""),
-         player_id = paste0("beta[", id, "]"))
-
-players <- users
-names(players) <- paste0("beta[", 1:length(users), "]")
-
-player_labels <- as_labeller(players)
-
-mcmc_hist(fit3_ave$draws(c("mu_beta",  "gamma1", "gamma2",
-                           "sigma_1", "tau[1]", "tau[2]",
-                           "sigma_g1", "sigma_g2")),
-          facet_args = list(scales = "free"))
-
-ggsave(filename = paste0(save_path, "/global_pars_all_rated_bullet_model.png"),
-                         width = 8, height = 8, units = "in")
-# ggsave(filename = paste0(save_path, "/global_pars_all_rated_blitz_model.png"),
-#        width = 8, height = 8, units = "in")
-
-theme_set(bayesplot_theme_get())
-
-random_effect_post |>
-  filter(param == 2) |>
-  ggplot(aes(value)) +
-  geom_histogram(fill = "#6497b1", colour = "black", size = 0.2) +
-  facet_wrap(~player_id, scales = "free",
-             labeller = player_labels, ncol = 5) +
-  labs(title = "Individual Winner Effects", y = "") +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank())
-
-
-# mcmc_hist(fit3_ave$draws("beta"),
-#           facet_args = list(labeller = player_labels))
-
-ggsave(filename = paste0(save_path, "/winner_pars_all_rated_bullet_model.png"),
-       width = 8, height = 8, units = "in")
-# ggsave(filename = paste0(save_path, "/winner_pars_all_rated_blitz_model.png"),
-#        width = 8, height = 8, units = "in")
-
-random_effect_post |>
-  filter(param == 1) |>
-  ggplot(aes(value)) +
-  geom_histogram(fill = "#6497b1", colour = "black", size = 0.2) +
-  facet_wrap(~player_id, scales = "free",
-             labeller = player_labels, ncol = 5) +
-  labs(title = "Individual Player Effects")
-
-ggsave(filename = paste0(save_path, "/indiv_pars_all_rated_bullet_model.png"),
-       width = 8, height = 8, units = "in")
-# ggsave(filename = paste0(save_path, "/indiv_pars_all_rated_blitz_model.png"),
-#        width = 8, height = 8, units = "in")
