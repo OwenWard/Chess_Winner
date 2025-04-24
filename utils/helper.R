8379fd88aa7b2e898a3be7e29a5d27a267a3d17d
AdamGee99 <arg15@sfu.ca>
2025-04-24 13:28:24 -0700

    fixing focal_win_prop calculation when prev_n > 1. History only includes the games in current session, up to a max of prev_n

--- utils/helper.R
+++ utils/helper.R
@@ -21,7 +21,6 @@ read_player <- function(path, file){
 get_hist <- function(user, games, prev_n) {
   if(prev_n == 1){
     hist_games <- games |> 
-      # filter(White == user | Black == user) |>
       filter(Username == user) |> 
       arrange(UTCDate, UTCTime) |> 
       mutate(focal_white = ifelse(Username == White, 1, 0)) |> 
@@ -37,21 +36,29 @@ get_hist <- function(user, games, prev_n) {
   }
   else{
     hist_games <- games |> 
-      # filter(White == user | Black == user) |> 
       filter(Username == user) |> 
-      arrange(UTCDate, UTCTime) |> 
-      mutate(focal_white = ifelse(Username == White, 1, 0)) |> 
-      select(White:BlackElo, focal_white) |> 
+      arrange(UTCDate, UTCTime) |>
+      mutate(focal_white = ifelse(Username == White, 1, 0),
+             UTCDateTime = ymd_hms(paste0(UTCDate, "_", UTCTime)),
+             time_diff = UTCDateTime - lag(UTCDateTime, default = NA)) |> 
+      select(White:BlackElo, focal_white, time_diff) |> 
       mutate(focal_result = case_when(
         (focal_white == 1 & Result == "1-0") ~ 1,
         (focal_white == 0 & Result == "0-1") ~ 1,
         (Result == "1/2-1/2") ~ 0.5,
-        .default = 0
-      )) |> 
-      mutate(focal_win_prop = c(cumsum(focal_result[1:(prev_n - 1)])/(1:(prev_n -1)), 
-                                roll_mean(focal_result, n = prev_n)))
+        .default = 0)
+      ) |> 
+      #updating so n = min(prev_n, number of games since first game in session)
+      mutate(n_session = (!(time_diff > 300 | is.na(time_diff))), #TRUE indicates game in a session, FALSE indicates first game in new session
+             n_session = 1 + accumulate(n_session, ~if (.y) {.x + 1} else {0}, .init = 0)[-1]) |> #the current number of games in a session
+      rowwise() |> 
+      mutate(n_hist = min(n_session, prev_n)) |> #the n to use for calculating history (ave_prop)
+      ungroup() |> #remove rowwise calculations
+      mutate(focal_win_prop = map_dbl(row_number(), function(i) { #rolling mean calculation (mean of past n_hist games)
+        start <- max(1, i - n_hist[i] + 1)
+        mean(focal_result[start:i])
+      }))
   }
-  
   hist_games
 }
 
