01b2e381169ffd8f70f0db4d84a5dc6383c5074e
OwenWard <owengward@gmail.com>
2025-05-05 11:01:45 -0700

    tidy this file a little bit

--- analysis_scripts/03_a_Fit_Perm_Session.R
+++ analysis_scripts/03_a_Fit_Perm_Session.R
@@ -104,11 +104,16 @@ init_data <- tidy_games |>
   select(focal_user, focal_id, focal_white, 
          focal_win_prop, elo_diff, focal_result, UTCDateTime) |> 
   group_by(focal_id) |>
-  mutate(time_diff = UTCDateTime - lag(UTCDateTime, default = NA), #default is to ensure first game is always start of a new session
-         cum_win_prob = cummean(focal_result), #the mean win probability for the focal player up to the ith (current) game 
+  mutate(time_diff = UTCDateTime - lag(UTCDateTime, default = NA), 
+         #default is to ensure first game is always start of a new session
+         cum_win_prob = cummean(focal_result), 
+         #the mean win probability for the focal player up to the ith (current) game 
          ave_prop = ifelse(time_diff > 300 | is.na(time_diff),  
-                           0, #if games played in different session, history is their mean win prob up to the current game
-                           lag(focal_win_prop) - cum_win_prob)) |> #if game played in same session, rolling mean over the past n games
+                           0, 
+                           #if games played in different session, 
+                           # history is their mean win prob up to the current game
+                           lag(focal_result) - cum_win_prob)) |> 
+  #if game played in same session, rolling mean over the past n games
   filter(focal_result != 0.5) %>%
   ungroup()
 
